df8e33b3a2f6e59180a59937bd723d349055fb43
shubham <aryashubh08@gmail.com>
2026-01-01 15:49:37 +0530

    fix bugs

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,4 @@
 export const USER_API_END_POINT =
-  "https://applicaton-tracker.vercel.app/v1/user";
+  "https://applicaton-tracker.vercel.app/api/v1/user";
 export const APPLICATION_API_END_POINT =
   "https://applicaton-tracker.vercel.app/api/v1/application";
